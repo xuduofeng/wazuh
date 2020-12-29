375b2e680a567d069c030542d4aafada5f930541
Jotacarma <jotacarma90@gmail.com>
2020-12-29 16:00:34 +0100

    Deleted static fields from syscheck: mode, hard_links, sk_tag, sym_path

--- src/analysisd/alerts/log.c
+++ src/analysisd/alerts/log.c
@@ -270,10 +270,10 @@ void OS_Log(Eventinfo *lf)
             fprintf(_aflog, "\nWhat changed:\n%s\n", lf->fields[FIM_DIFF].value);
         }
 
-        if (lf->sk_tag) {
-            if (strcmp(lf->sk_tag, "") != 0) {
+        if (lf->fields[FIM_TAG].value) {
+            if (strcmp(lf->fields[FIM_TAG].value, "") != 0) {
                 char * tags;
-                os_strdup(lf->sk_tag, tags);
+                os_strdup(lf->fields[FIM_TAG].value, tags);
                 fprintf(_aflog, "\nTags:\n");
                 char * tag;
                 tag = strtok_r(tags, ",", &saveptr);
