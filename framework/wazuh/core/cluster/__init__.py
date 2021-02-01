2a2d6ec4ba564a8592a22054a38e67e921b66ca1
David Jose Iglesias Lopez <davidj.iglesias@wazuh.com>
2021-02-01 16:10:01 +0100

    Bump rev 40012

--- framework/wazuh/core/cluster/__init__.py
+++ framework/wazuh/core/cluster/__init__.py
@@ -5,7 +5,7 @@
 # This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
 
 __version__ = '4.0.4'
-__revision__ = '40011'
+__revision__ = '40012'
 __author__ = "Wazuh Inc"
 __ossec_name__ = "Wazuh"
 __licence__ = "\
